e335b62a86567108dddb12b5228f80c161841ec5
tsimenis <tsimenis@gmail.com>
2019-08-05 13:46:27 +0200

    rename imports

--- cli.js
+++ cli.js
@@ -2,42 +2,52 @@
 
 const defaults = require('./src/defaults')
 const figmaApiBase = 'https://api.figma.com/v1'
-const Fs = require('fs')
-const Path = require('path')
+const fs = require('fs')
+const path = require('path')
 const ora = require('ora')
 const chalk = require('chalk')
-const Axios = require('axios')
+const axios = require('axios')
 const prompts = require('prompts')
 const promptsList = require('./src/prompts')
 const mkdirp = require('mkdirp')
 let config = {}
+const spinner = ora()
+
+axios.interceptors.request.use((conf) => {
+  conf.headers = {
+    'Content-Type': 'application/json',
+    'X-Figma-Token': config.figmaPersonalToken
+  }
+  conf.startTime = new Date().getTime()
+  return conf
+})
 
 function deleteConfig () {
-  const configFile = Path.resolve(defaults.configFileName)
-  if (Fs.existsSync(configFile)) {
-    Fs.unlinkSync(configFile)
+  const configFile = path.resolve(defaults.configFileName)
+  if (fs.existsSync(configFile)) {
+    fs.unlinkSync(configFile)
     console.log(chalk.cyan.bold('Deleted previous config'))
   }
 }
 
 function updateGitIgnore () {
   const ignorePath = '.gitignore'
-  const ignoreCompletePath = Path.resolve(ignorePath)
+  const ignoreCompletePath = path.resolve(ignorePath)
   const ignoreContent = '\n#figma-export-icons\nicons-config.json'
-  const ignore = Fs.existsSync(ignoreCompletePath)
-    ? Fs.readFileSync(ignoreCompletePath, 'utf-8')
+  const ignore = fs.existsSync(ignoreCompletePath)
+    ? fs.readFileSync(ignoreCompletePath, 'utf-8')
     : ''
   if(!ignore.includes(ignoreContent)) {
-    Fs.writeFileSync(ignoreCompletePath, ignore + ignoreContent)
+    fs.writeFileSync(ignoreCompletePath, ignore + ignoreContent)
     console.log(`Updated ${ignorePath} : ${ignoreContent}`)
   }
 }
 
 function getConfig () {
   return new Promise((resolve) => {
-    const configFile = Path.resolve(defaults.configFileName)
-    if (Fs.existsSync(configFile)) {
-      config = JSON.parse(Fs.readFileSync(configFile, 'utf-8'))
+    const configFile = path.resolve(defaults.configFileName)
+    if (fs.existsSync(configFile)) {
+      config = JSON.parse(fs.readFileSync(configFile, 'utf-8'))
       const missingConfig = promptsList.filter((q) => !config[q.name])
       if (missingConfig.length > 0) getPromptData(missingConfig).then(() => resolve())
       else resolve()
@@ -53,35 +63,25 @@ async function getPromptData ( list = promptsList ) {
   }
   const response = await prompts(list, { onCancel })
   config = Object.assign(config, response)
-  Fs.writeFileSync('icons-config.json', JSON.stringify(config, null, 2))
+  fs.writeFileSync('icons-config.json', JSON.stringify(config, null, 2))
 }
-
-Axios.interceptors.request.use((conf) => {
-  conf.headers = {
-    'Content-Type': 'application/json',
-    'X-Figma-Token': config.figmaPersonalToken
-  }
-  conf.startTime = new Date().getTime()
-  return conf
-})
-const spinner = ora()
 
 function deleteIcons () {
   return new Promise((resolve) => {
-    const directory = Path.resolve(config.iconsPath)
-    if (!Fs.existsSync(directory)){
+    const directory = path.resolve(config.iconsPath)
+    if (!fs.existsSync(directory)){
       console.log(`Directory ${config.iconsPath} does not exist`)
       if (mkdirp.sync(directory)) {
         console.log(`Created directory ${config.iconsPath}`)
         resolve()
       }
     } else {
-      Fs.readdir(directory, (err, files) => {
+      fs.readdir(directory, (err, files) => {
         if (err) throw err
         spinner.start('Deleting directory contents')
         files.forEach((file) => {
           if (file !== 'README.md') {
-            Fs.unlinkSync(Path.join(directory, file))
+            fs.unlinkSync(path.join(directory, file))
           }
         })
         spinner.succeed()
@@ -105,7 +105,7 @@ function findDuplicates (propertyName, arr) {
 function getFigmaFile () {
   return new Promise((resolve) => {
     spinner.start('Fetching Figma file (this might take a while depending on the figma file size)')
-    Axios.get(`${figmaApiBase}/files/${config.fileId}`)
+    axios.get(`${figmaApiBase}/files/${config.fileId}`)
       .then((res) => {
         const endTime = new Date().getTime()
         spinner.succeed()
@@ -141,7 +141,7 @@ function getImages (icons) {
   return new Promise((resolve) => {
     spinner.start('Fetching icon urls')
     const iconIds = icons.map(icon => icon.id).join(',')
-    Axios.get(`${figmaApiBase}/images/${config.fileId}?ids=${iconIds}&format=svg`)
+    axios.get(`${figmaApiBase}/images/${config.fileId}?ids=${iconIds}&format=svg`)
       .then((res) => {
         spinner.succeed()
         const images = res.data.images
@@ -158,11 +158,11 @@ function getImages (icons) {
 }
 
 function downloadImage (url, name) {
-  const path = Path.resolve(config.iconsPath, `${name}.svg`)
-  const writer = Fs.createWriteStream(path)
+  const imagePath = path.resolve(config.iconsPath, `${name}.svg`)
+  const writer = fs.createWriteStream(imagePath)
 
 
-  Axios.get(url, {responseType: 'stream'})
+  axios.get(url, {responseType: 'stream'})
     .then((res) => {
       res.data.pipe(writer)
     })
