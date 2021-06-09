c1ecc50fdd5b761ddd36b45aec4b6ed3b482fa87
Leonidas Esteban <leonidasesteban@gmail.com>
2021-06-09 15:34:25 -0500

    [feature] remove from name
    remove config.removeFromName from the icon name, useful to remove designer prefix to organice their components

--- src/defaults.js
+++ src/defaults.js
@@ -2,7 +2,8 @@ const defaults = {
   configFileName: 'icons-config.json',
   page: 'Identity',
   frame: 'Icons',
-  iconsPath: 'assets/svg/icons'
+  iconsPath: 'assets/svg/icons',
+  removeFromName: 'Icon='
 }
 
 module.exports = defaults
